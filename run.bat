b69287d5ec033cafde03606e005c1557301953bd
Luisfetoga2 <l.torresg@uniandes.edu.co>
2024-11-13 19:09:20 -0500

    Optimized bat files

--- run.bat
+++ run.bat
@@ -4,4 +4,4 @@
 start cmd /k "cd front && npm start"
 
 :backend
-start cmd /k "cd back && pip install -r requirements.txt && python -m uvicorn main:app --reload"
+start cmd /k "cd back && python -m uvicorn main:app --reload"
--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 @echo off
 
-start cmd /k "git pull && cd front && npm install && exit"
+start cmd /c "git pull && cd front && npm install && cd ../back && pip install -r requirements.txt && exit"
